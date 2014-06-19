f570f372aeffe9a7c9ff15b2b3f61554628aa37a
Jason Baier <jason.baier@outlook.com>
2014-06-19 10:15:50 -0700

    Switching DB driver to mysqli

--- application/config/database.php
+++ application/config/database.php
@@ -50,9 +50,9 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
+$db['default']['password'] = '';
 $db['default']['database'] = 'ci-fire-starter';
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
